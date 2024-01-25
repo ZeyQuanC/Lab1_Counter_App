480288369017110649dd618da0a22fb42c7c8889
ZeyQuanC <ZeyQuan40@gmail.com>
2024-01-25 11:51:22 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lab1counterapp/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.lab1counterapp
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+var counter = 0
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        val upgradeButton = findViewById<Button>(R.id.upgradeBtn)
+
+        button.setOnClickListener {
+            // Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
+            counter++
+            textView.text = counter.toString()
+
+            if (counter >= 100) {
+
+                // Show upgrade button and set onClickListener
+                upgradeButton.visibility = View.VISIBLE
+                upgradeButton.setOnClickListener {
+                    button.text = "Add 2"
+
+                    // Update original button to add 2 instead of `
+                    button.setOnClickListener {
+                        counter += 2
+                        textView.text = counter.toString()
+                    }
+
+                    // Hide upgrade button again
+                    upgradeButton.visibility = View.INVISIBLE
+                }
+            }
+        }
+        button.setOnClickListener {
+            // Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
+            counter++
+            textView.text = counter.toString()
+
+            if (counter >= 500) {
+
+                // Show upgrade button and set onClickListener
+                upgradeButton.visibility = View.VISIBLE
+                upgradeButton.setOnClickListener {
+                    button.text = "Add 5"
+
+                    // Update original button to add 2 instead of `
+                    button.setOnClickListener {
+                        counter += 2
+                        textView.text = counter.toString()
+                    }
+
+                    // Hide upgrade button again
+                    upgradeButton.visibility = View.INVISIBLE
+                }
+            }
+        }
+
+    }
+}
